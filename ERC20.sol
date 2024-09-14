1094015f0e65f0227181b96efe223255ae5e3f1c
Abhigyan Pushkar <149153539+Abhiii1234@users.noreply.github.com>
2024-09-14 11:24:56 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract radiant_points is ERC20 {
-    constructor(uint256 initialBalance) ERC20("radiant_points","v"){
+contract radix_points is ERC20 {
+    constructor(uint256 initialBalance) ERC20("radix_points","v"){
         _mint(msg.sender, initialBalance);
     }
 }
