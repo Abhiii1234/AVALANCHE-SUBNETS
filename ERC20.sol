2fa61b8285b3e065612341ccff722ba25fcb0861
Abhigyan Pushkar <149153539+Abhiii1234@users.noreply.github.com>
2024-09-14 11:24:04 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
+pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Emerald is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Emerald", "EMD") {
-        _mint(msg.sender, initialSupply);
+
+contract radiant_points is ERC20 {
+    constructor(uint256 initialBalance) ERC20("radiant_points","v"){
+        _mint(msg.sender, initialBalance);
     }
 }
